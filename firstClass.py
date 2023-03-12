e0b53e7e62c2d1ea530e75e855285ca7210aa357
fastoch <fastoch@ik.me>
2023-03-12 06:41:18 +0100

    method calculateTotalPrice

--- firstClass.py
+++ firstClass.py
@@ -1,7 +1,9 @@
-# Create a class and instantiate some object of this class
+# Create a class
 class Item:
-    pass
+    def calculateTotalPrice(self, x, y):
+        return x * y
 
+# class instantiation (= object creation)
 item1 = Item()
 
 # Create some attributes for our object
@@ -10,7 +12,26 @@ item1.price = 600
 item1.color = "red"
 item1.quantity = 8
 
-print(type(item1)) # Item
+print(type(item1)) # __main__.Item
 print(type(item1.name)) # str
 print(type(item1.price)) # int
 print(type(item1.quantity)) # int
+
+# an object is an instance of a class
+# attributes are the object properties
+
+# String is a built-in class, including multiple built-in methods
+random_str = "aaa"
+print(random_str.upper()) # AAA
+
+# Methods must be defined inside our class
+# Methods are functions inside classes
+
+# Python passes the object itself as a first argument when you call a method, 
+# which is why you always see a default parameter named 'self' when you declare a method
+# using the word self is a common convention. It's best practise to use this specific word
+
+# arguments are what you pass to a method while invoking it
+# parameters are what you pass to a method while declaring it
+
+print(item1.calculateTotalPrice(item1.price, item1.quantity))
