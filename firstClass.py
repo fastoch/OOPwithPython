69f049a99d15471068cc0bf34513224bf3150100
fastoch <fastoch@ik.me>
2023-03-12 06:07:36 +0100

    first commit

---
+++ firstClass.py
@@ -0,0 +1,12 @@
+# Create a class and instantiate some object of this class
+class Item:
+    pass
+
+item1 = Item()
+
+# Create some attributes for our object
+item1.name = "Phone"
+item1.price = 600
+item1.color = "red"
+item1.quantity = 8
+
