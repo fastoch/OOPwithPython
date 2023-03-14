cf7508f18a2202b151a5aa32961b53bf5a6f8f9a
fastoch <fastoch@ik.me>
2023-03-14 19:10:39 +0100

    custom pay rate for a specific instance

--- constructor__init__v2.py
+++ constructor__init__v2.py
@@ -18,11 +18,17 @@ class Item:
     def totalPrice(self):
         return self.price * self.quantity
     
+    def applyDiscount(self):
+        self.price = self.price * self.pay_rate # applying 20% discount
+        self.price = round(self.price, 2) # rounding down to 2 decimals
+
 
 item1 = Item("honeypot", 12.0, 4)
-
-print(f"{item1.totalPrice()}€")
-print(item1.name)
+item1.applyDiscount()
+print(f"Item1: {item1.name}")
+print(f"Discount unit price: {item1.price}€")
+print(f"Discount Total price: {item1.totalPrice()}€")
+print()
 
 # So far, we worked with "instance attributes"
 # There is another kind of attributes that we call "Class Attributes"
@@ -30,3 +36,24 @@ print(item1.name)
 
 print(Item.pay_rate) # accessing from the class level
 print(item1.pay_rate) # accessing from the instance level
+print()
+
+# Using a built-in magic attribute that displays all attributes belonging to a class|object
+print("Attributes belonging to the Item class:")
+print(Item.__dict__)
+print("\nAttributes belonging to the item1 object:")
+print(item1.__dict__)
+
+
+# just testing the round() function
+print(f"\n{round(25.600002, 2)}")
+print(f"{round(25.638562, 2)}\n")
+
+# customizing pay rate for a specific instance
+item2 = Item("Pepper", 3.5, 5)
+print(f"Item2: {item2.name}")
+item2.pay_rate = 0.6
+print(f"Item2 pay rate: {item2.pay_rate}")
+item2.applyDiscount()
+print(f"Discount unit price: {item2.price}€")
+print(f"Discount Total price: {item2.totalPrice()}€")
