54e1fd230f6d07a7bc8a85c5b783a376a8cce106
fastoch <fastoch@ik.me>
2023-03-18 18:44:18 +0100

    modifying item1.name

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # This file will be used to instantiate different classes (create new objects)
 from item import Item
-from phone import Phone
 
 Item.instantiate_from_csv()
 print(Item.all)                 # checking if instantiation from .csv file actually worked
+
+item1 = Item("MyItem", 750)     # I don't specify quantity because we have a default value of 0
+item1.name = "OtherItem"
+
+print(item1.name)
