ac7b6d3410e71952f03466d9bc73836f2994ef82
fastoch <fastoch@ik.me>
2023-03-18 10:16:42 +0100

    creating files 6, 7 and 8

--- 5.inheritance.py
+++ 5.inheritance.py
@@ -24,7 +24,8 @@ class Item:
         self.price = round(self.price, 2) 
     
     def __repr__(self):
-        return f"Item({self.name}, {self.price}, {self.quantity})" 
+        return f"{self.__class__.__name__}({self.name}, {self.price}, {self.quantity})" 
+        # self.__class__.__name__ allows us to access the name of the class (to which belongs the instance)
     
     @classmethod 
     def instantiate_from_csv(cls):
@@ -55,13 +56,12 @@ class Item:
 # This method will not be useful for any instance of the Item class, because it only applies to phones
 # To solve this, we can create a separated class that will inherit the functionalities of the Item class
 
-# The Phone class will inherit all the attributes and methods from the Item class
+# The Phone class will inherit all attributes and methods from the Item class, including the list appending (line 16)
 class Phone(Item):
-    all = []
     def __init__(self, name:str, price:float, quantity=0, broken_phones=0):
-        # Call to super function to inherit all attributes and methods from the parent class
-        super().__init__(
-            name, price, quantity
+        # Call to super function to inheritk all attributes and methods from the parent class
+        super().__init__(           # This calls the __init__ method in the parent class
+            name, price, quantity   # This imports attributes from the parent class (Item) to the child class (Phone)
         )
 
         # Run validations to the received arguments
@@ -70,15 +70,15 @@ class Phone(Item):
         # Assign to self object
         self.broken_phones = broken_phones
 
-        # Actions to execute
-        Phone.all.append(self)
-
 phone1 = Phone("Moto_v10", 500, 5)
 phone2 = Phone("Moto_v20", 700, 8)
 
 # using a method declared in the parent class (Item)
 print(phone1.totalPrice())
 print(phone2.totalPrice()) 
+
+# Note that I replaced 'Item' with '{self.__class__.__name__}' in the __repr__ method (line 27)
+print(Item.all)     # same result as print(Phone.all)
 
 # Phone is the child class of Item
 # Item is the parent class of Phone
